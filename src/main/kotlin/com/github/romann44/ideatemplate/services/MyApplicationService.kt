0747e3fdca8f42c80ef78486cddeed55c18effce
GitHub Action <action@github.com>
2022-08-03 06:29:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/romann44/ideatemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/romann44/ideatemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.romann44.ideatemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.romann44.ideatemplate.MyBundle
 
 class MyApplicationService {
 
--- src/test/kotlin/com/github/romann44/ideatemplate/MyPluginTest.kt
+++ src/test/kotlin/com/github/romann44/ideatemplate/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.romann44.ideatemplate
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
